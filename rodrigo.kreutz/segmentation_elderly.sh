102389c4337fde85e4776b2729f9c6353aae6fcc
Rodrigo Kreutz <rkreutz@marfim.(none)>
2014-09-30 16:15:44 -0300

    Foi adicionado for loop para entrar nas pastas dos subjs e suas visitas. Nao removemos nenhum arquivo por seguranca. Caso nao exista o arquivo MNI na pasta nos copiamos do home user. A compressao foi alterada para ser a melhor ao inves da mais rapida.

--- rodrigo.kreutz/segmentation_elderly.sh
+++ rodrigo.kreutz/segmentation_elderly.sh
@@ -1,36 +1,48 @@
 #!/bin/bash
 
-cd ~/DATA/MCRUCIUS/CAT007/visit1/ANAT/TESTAR_SS
+study=CAT
+subjs=(007 008 009 010 011)
+visits=(visit1 visit2)
 
-if [ ! -f ./$1+orig.HEAD ]; then
-	3dSkullStrip -input CAT007.ANAT.nii.gz 	\
-		     -no_touchup 		\
-		     -shrink_fac_bot_lim 0.65 	\
-		     -use_skull			\
-		     -perc_int 0.1		\
-		     -no_pushout		\
-		     -orig_vol			\
-		     -avoid_vent -avoid_vent	\
-		     -init_radius 75		\
-		     -prefix $1
-	rm *.ply
-else
-	echo "Arquivo Skullstrip ja existe."
-fi
+for subj in ${subjs[*]} ; do
+	for visit in ${visits[*]} ; do
+		cd ~/DATA/MCRUCIUS/$study$subj/$visit/ANAT
 
-if [ ! -f ./$1+tlrc.HEAD ]; then
-	@auto_tlrc -base MNI_caez_N27+tlrc -input $1+orig -no_ss
-	3dQwarp	-workhard			\
- 		-maxlev 7			\
- 		-nowarp				\
-		-blur 0 3			\
-		-overwrite			\
- 		-base MNI_caez_N27+tlrc.HEAD	\
- 		-source $1+tlrc.HEAD		\
-		-prefix $1
-	3dmerge -1noneg -1clip 10 -prefix $1 -overwrite $1+tlrc.HEAD
-else
-	echo "Arquivo TLRC ja existe."
-fi
+		if [ ! -f ./MNI_caez_N27+tlrc.HEAD ]; then
+			cp ~/neuro-scripts/rodrigo.kreutz/MNI_caez_N27+tlrc.BRIK.gz ./
+			cp ~/neuro-scripts/rodrigo.kreutz/MNI_caez_N27+tlrc.HEAD ./
+		fi
 
-gzip -1v *.BRIK
+		if [ ! -f ./NS+orig.HEAD ]; then
+			3dSkullStrip -input $study$subj.ANAT.nii.gz 	\
+		     		     -no_touchup 			\
+		     		     -shrink_fac_bot_lim 0.65 		\
+		     		     -use_skull				\
+		     		     -perc_int 0.1			\
+		     		     -no_pushout			\
+		     		     -orig_vol				\
+		     		     -avoid_vent -avoid_vent		\
+		     		     -init_radius 75			\
+		     		     -prefix NS
+		else
+			echo "Arquivo Skullstrip ja existe."
+		fi
+
+		if [ ! -f ./NS+tlrc.HEAD ]; then
+			@auto_tlrc -base MNI_caez_N27+tlrc -input NS+orig -no_ss
+			3dQwarp	-workhard			\
+ 				-maxlev 7			\
+ 				-nowarp				\
+				-blur 0 3			\
+				-overwrite			\
+ 				-base MNI_caez_N27+tlrc.HEAD	\
+ 				-source NS+tlrc.HEAD		\
+				-prefix NS
+			3dmerge -1noneg -1clip 10 -prefix NS -overwrite $1+tlrc.HEAD
+		else
+			echo "Arquivo TLRC ja existe."
+		fi
+	done
+done
+
+gzip -9v *.BRIK
